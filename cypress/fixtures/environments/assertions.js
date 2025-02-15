f10e4e485782cac2f1d51a2904c0b33bab0f6919
Vladimir Cupkovic <cupkovic17@yahoo.com>
2025-02-15 14:54:30 +0100

    updated assertions.js file

--- cypress/fixtures/environments/assertions.js
+++ cypress/fixtures/environments/assertions.js
@@ -1,6 +1,10 @@
 export const Warnings = {
     emptyEmailWarning: 'The email field is required.',
     emptyPasswordWarning: 'The password field is required.',
+    emptyUsernameWarning: 'The username field is required.',
     invalidCredentialsWarning: 'The email address or password you entered is invalid',
-    wrongEmailFormatWarning: 'The email field must be a valid email address.'
+    wrongEmailFormatWarning: 'The email field must be a valid email address.',
+    usedEmailWarning: 'The email has already been taken.',
+    usedUsernameWarning: 'The username has already been taken.',
+    notEnoughCharPasswordWarning: 'The password field must be at least 6 characters.'
   };
