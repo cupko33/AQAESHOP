719e2f5230bf1bd86c88a8d2c64888251418f226
Vladimir Cupkovic <cupkovic17@yahoo.com>
2025-02-15 13:55:49 +0100

    test for login form when email format is wrong

---
+++ cypress/e2e/loginTests/loginFormWrongEmailFormat.cy.js
@@ -0,0 +1,27 @@
+import { URLs } from '../../fixtures/environments/links';
+import { LoginPage } from '../../support/pageObject/login';
+import { HomePage } from '../../support/pageObject/homePage';
+import { Warnings } from '../../fixtures/environments/assertions';
+import { LoginCredentials } from '../../fixtures/environments/testData';
+
+
+describe('Wrong Email Format in Login', () => {
+  it('should validate warning displayed when email has wrong format', () => {
+
+  const loginPage = new LoginPage();
+  const homePage = new HomePage();
+  
+  cy.visit(URLs.base);
+  homePage.logIn().click();
+  
+  loginPage.emailField().type(LoginCredentials.wrongEmailFormat);
+  loginPage.passwordField().type(LoginCredentials.validPassword);
+  loginPage.signInButton().click();
+  loginPage.warningText().should('be.visible');
+  loginPage.warningText().contains(Warnings.wrongEmailFormatWarning);
+
+  cy.window().then((win) => {
+    win.close();
+    });
+  });
+});
