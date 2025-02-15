e377900c1f371e4be0b7b4ab6af36fbb614797e5
Vladimir Cupkovic <cupkovic17@yahoo.com>
2025-02-15 15:25:35 +0100

    test for already used email in registration

---
+++ cypress/e2e/registrationTests/duplicateEmailRegistration.cy.js
@@ -0,0 +1,46 @@
+import { URLs } from '../../fixtures/environments/links';
+import { HomePage } from '../../support/pageObject/homePage';
+import { Registration } from '../../support/pageObject/registration';
+import { generateUser } from '../../fixtures/environments/testData';
+import { Warnings } from '../../fixtures/environments/assertions';
+
+
+describe('User Registration - duplicate email', () => {
+  it('should show warning when email is already used', () => {
+
+  const homePage = new HomePage();
+  const registrationPage =  new Registration;
+  const user = generateUser();
+
+    cy.visit(URLs.base);
+    homePage.register.click();
+
+    // Register first user
+    registrationPage.usernameField().type(user.name);
+    registrationPage.emailField().type(user.email);
+    registrationPage.passwordField().type(user.password);
+    
+    // Submit the form and validate page
+    registrationPage.registerButton().click();
+    cy.url().should('eq', URLs.dashboard);
+
+    //Logout
+    dashboard.hamburger().click();
+    dashboard.logout().click();
+
+    // Register with the same email but a different username
+    registrationPage.usernameField().type(`New${user.name}`);
+    registrationPage.emailField().type(user.email);
+    registrationPage.passwordField().type(user.password);
+    registrationPage.registerButton().click();
+
+    // Validate warning shown
+    registrationPage.warningText().contains(Warnings.usedEmailWarning)
+    
+    // Close window
+    cy.window().then((win) => {
+    win.close();
+    }); 
+  });
+});
+  
